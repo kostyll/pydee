683837bf3cdd3fa737cbb5315e49a62c8e2f7ed6
Pierre Raybaut <pierre.raybaut@gmail.com>
2009-05-31 08:42:54 +0200

    Added very simple example

---
+++ example.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    Copyright © 2009 Pierre Raybaut
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#    
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+Simple formlayout example
+
+Please take a look at formlayout.py for more examples
+(at the end of the script, after the 'if __name__ == "__main__":' line)
+"""
+
+from formlayout import fedit
+
+datalist = [('Name', 'Paul'),
+            ('Age', 30),
+            ('Sex', [0, 'Male', 'Female']),
+            ('Size', 12.1),
+            ('Eyes', 'green'),
+            ('Married', True),
+            ]
+
+print "result:", fedit(datalist, title="Describe yourself",
+                       comment="This is just an <b>example</b>.")
